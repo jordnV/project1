63d95cb5d019cf899c62dc6543894c0e0ffe19ec
jordnV <jord.vanessa@gmail.com>
2015-03-01 02:15:35 +0000

    initial commit to project1

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from . import db 
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key = 'True')
+  firstname = db.Column(db.String(80))
+  lastname = db.Column(db.String(80))
+  pimage =  db.Column(db.String(80))
+  sex = db.Column(db.String(80))
+  age = db.Column(db.Integer)
+  
+  def __init__(self, firstname,lastname,image,sex,age):
+    self.firstname = firstname
+    self.lastname = lastname
+    self.pimage = pimage
+    self.sex = sex
+    self.age = age
+    
+  def __repr__(self):
+    return'<User%r>'%self.firstname
